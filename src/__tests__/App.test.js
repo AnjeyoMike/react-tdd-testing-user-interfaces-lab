9580a117506c3654d703b42f9ba7714053639fdb
Anjeyo Mike <mike.matekeya@student.moringaschool.com>
2024-06-22 22:54:53 +0300

    My Job

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,9 @@
 import { render, screen } from "@testing-library/react";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 
 import App from "../App";
 
 // Your tests here
+test("placeholder test", () => {
+  expect(true).toBe(true);
+});
